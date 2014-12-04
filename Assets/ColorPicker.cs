b7d326d0a01e3dd11380cb7a77e004976961061b
Samuel Eng <sam-eng@icloud.com>
2014-12-04 14:47:07 -0500

    init

---
+++ Assets/ColorPicker.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorPicker : MonoBehaviour {
+
+	private MouseOrbit orb;
+	public bool pickingColor = false;
+	private GameObject cubePicker;
+
+
+	void Start () {
+		orb = ColorSpawner.main.orb;
+		cubePicker = (GameObject)Instantiate(ColorSpawner.main.spawnObj, new Vector3(-30f, -30f, -30f), Quaternion.identity);
+		cubePicker.transform.renderer.material.color = Color.black;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F)){
+			orb.target = cubePicker.transform;
+			orb.distance = 1f;
+			pickingColor = true;
+		}
+
+
+		if (pickingColor){
+			Debug.Log("???");
+			float speed = 10f * Time.deltaTime;
+
+			Color c = cubePicker.renderer.material.color;
+
+			if (Input.GetKey(KeyCode.Space)){
+				c = new Color(Random.Range(0, 1f),
+				              Random.Range(0, 1f),
+				              Random.Range(0, 1f));
+			}
+
+			cubePicker.renderer.material.color = c;
+
+			if (Input.GetKeyDown(KeyCode.L)){
+				LocateColor(cubePicker.renderer.material.color);
+			}
+		}
+	}
+
+	void LocateColor(Color c){
+		GameObject closest = ColorSpawner.main.tree.nearest(c.GetKey()) as GameObject;
+		cubePicker.GetComponent<CubePositionFader>().target = closest.transform.position + Camera.main.transform.forward * 2f;
+		orb.target = closest.transform;
+		orb.distance = 5f;
+	}
+}
---
+++ Assets/ColorSpawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using KDTreeDLL;
+
+public class ColorSpawner : MonoBehaviour {
+	public static ColorSpawner main;
+
+	public GameObject spawnObj;
+	public KDTree tree{get; private set;}
+	public MouseOrbit orb;
+
+	void Awake(){main = this;}
+
+	// Use this for initialization
+	void Start () {
+		tree = new KDTree(3);
+		int steps = 5;
+		int spacing = 6;
+		GameObject cubes = new GameObject("Cubes");
+		for(int i = 0; i < steps; i++){
+			for(int j = 0; j < steps; j++){
+				for(int k = 0; k < steps; k++){
+					float s = (float) steps;
+					Color c = new Color(i / s, j / s, k / s);
+					GameObject cube = (GameObject)Instantiate(spawnObj, new Vector3(i * spacing, j * spacing, k * spacing), Quaternion.identity);
+					cube.transform.parent = cubes.transform;
+					cube.renderer.material.color = c;
+					tree.insert(c.GetKey(), cube);
+				}
+			}
+		}
+		float center = ((steps-1) / 2f)*spacing;
+		cubes.transform.Translate(-new Vector3(center, center, center));
+		orb.target = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+}
+
+public static class ExtensionMethods{
+
+	public static double[] GetKey(this Color c){
+		double[] keys = new double[3];
+		keys[0] = c.r;
+		keys[1] = c.g;
+		keys[2] = c.b;
+		return keys;
+	}
+}
+
+
